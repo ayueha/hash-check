8c203d339b5933ec3b94c376c24ed97f2150ca9c
asavapi <ayueha@ttu.ee>
2019-08-02 17:20:20 +0900

    commit a script

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-if [$# -ne 1]; then
-    echo "p : python での実行を指定" 1>&2
-	exit 1
+if [ $# -ne 1 ]; then
+    echo "directory is mandatory in first option" 1>&2
+    #echo "p or P : Choose python script option" 1>&2
+    exit 1
+else
+    if [$2 = "p" -o $2="P" -o $2=""]; then
+        inotifywatch -e create -mq $1 | /usr/bin/python3 ./scripts/hash.py 
+    fi
 fi
-	
-if [$1 -eq "p" || $1 -eq "P" ]; then
-    cd scripts
-    /usr/bin/python3 hash.py
-fi
+
+#elif [ $1 = "p" -o $1 = "P" ]; then
+#    cd scripts
+#    /usr/bin/python3 hash.py
+#fi
 	
