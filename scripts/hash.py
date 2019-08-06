15f57ae59eee05b96adbe091482fce3e0491a1ee
ayueha <ayueha@ttu.ee>
2019-08-06 10:59:45 +0300

    add file hash and compair with current db information

--- scripts/hash.py
+++ scripts/hash.py
@@ -32,9 +32,10 @@ def parser():
     :return:args.attributes
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('attributes', help='[i] initialize hash database ')
+    parser.add_argument('attributes', help='[i] initialize hash database, [d] create hash under a directory, [f] scan a file')
+    parser.add_argument('path', help='mandatory for directory or file hash creating')
     args = parser.parse_args()
-    return args.attributes
+    return args.attributes, args.path
 
 
 class HashScan():
@@ -44,25 +45,43 @@ class HashScan():
     file: target file
     """
 
-    def __init__(self, option):
+    def __init__(self, option, path):
         self.option = option
-        self.path = ''
+        self.path = path
+        self.db = DatabaseInfo()
 
-    def initial_database(self):
+    def create_info(self):
         """
         scanning : /usr/bin /home /tmp /opt
         and import hash info via sql script
         :return:
         """
-        homedir = os.path.expanduser("~")
-        pathArray = [homedir, "/tmp/", "/opt/"]
-        for d in pathArray:
-            files = os.listdir(d)
-            for f in files:
-                if os.path.isfile(os.path.join(d, f)) and os.path.getsize(os.path.join(d, f)):
-                    self.import_hash(self.hash_string(os.path.join(d, f)), os.path.join(d, f))
+        if self.option == "i":
+            homedir = os.path.expanduser("~")
+            pathArray = [homedir, "/tmp/", "/opt/"]
+            for d in pathArray:
+                files = os.listdir(d)
+                for f in files:
+                    if self.check_file(os.path.join(d, f)):
+                        self.import_hash(self.hash_string(os.path.join(d, f)), os.path.join(d, f))
+        elif self.option == "f" and self.path !="":
+            if self.check_file(self.path):
+                record_number=self.db.find_hash(self.db.create_connection(),self.path)
+            if record_number ==1 :
+                print ('Current hash record has found')
+                record_info = self.db.select_hash(self.db.create_connection(),self.path)
+            elif record_number == 0:
+                print ('Check new file hash info')
+                if self.check_file(self.path):
+                    self.import_hash(self.hash_string(self.path), self.path)
 
-        """files = os.listdir(path)"""
+
+    def check_file(self,filepath):
+        if os.path.isfile(filepath) and os.path.getsize(filepath):
+            return True
+        else:
+            return False
+
 
     def hash_string(self, filename, block_size=56636):
         """
@@ -82,8 +101,7 @@ class HashScan():
         import hash (sha256) and file name
         :return:None
         """
-        db = DatabaseInfo()
-        db.insert_column(db.create_connection(), hash, filename)
+        self.db.insert_column(self.db.create_connection(), hash, filename)
 
 
 class DatabaseInfo():
@@ -110,8 +128,29 @@ class DatabaseInfo():
         cur.commit()
         cur.close()
 
+    def find_hash (self,cur,filename):
+        sqlstring = 'select count() as record_number from INIT_HASH where FILE_NAME =' + '"' + filename + '"'
+        cur.execute(sqlstring)
+        record_number = cur.fetchall()
+
+        return record_number
+
+    def select_hash(self,cur,filename):
+        sqlstring = 'select FILE_NAME, HASH, SCANED_FLAG, INSERTED_DATE, UPDATED_DATE from INIT_HASH where FILE_NAME =' + '"' + filename + '"'
+        cur.execute(sqlstring)
+        detail_hash = cur.fetchall()
+        return detail_hash
 
 if __name__ == '__main__':
-    scan = HashScan(parser())
-    scan.initial_database()
+    options = parser()
+    scan = HashScan(options[0],options[1])
+    if scan.option == 'i':
+        scan.create_info()
+    elif scan.option == 'd' or scan.option == 'f':
+        if scan.path != "":
+            scan.create_info()
+        else:
+            print('Lack of file path information \n')
+
+
 
