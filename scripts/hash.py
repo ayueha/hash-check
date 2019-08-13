5401801a68b08e2379d898a6740088c1860714a8
ayueha <ayueha@ttu.ee>
2019-08-13 15:58:48 +0300

    Initial database creatation test finished. Add exception in SQLite3 process

--- scripts/hash.py
+++ scripts/hash.py
@@ -23,13 +23,13 @@ import hashlib
 import sqlite3
 import datetime
 import socket
-import glob
 from hmac import compare_digest
 
 
 """GLOBAL"""
-DB_PATH = "../database/hashmaster"
-SERVER_IP = "192.168.10.30"
+DB_PATH = '../database/hashmaster'
+SERVER_IP = '192.168.10.30'
+LOG_PATH = '../log'
 
 
 def parser():
@@ -68,24 +68,24 @@ class HashScan():
         """
         if self.option == "i":
             homedir = os.path.expanduser("~")
-            pathArray = [homedir, "/tmp/", "/opt/"]
+            pathArray = ["/tmp/", homedir]
 
             for self.path in pathArray:
                 for f in self.find_files():
-                    if self.check_file(os.path.join(self.path, f)):
-                        self.db.insert_column(self.hash_string(os.path.join(self.path, f)), os.path.join(self.path, f),
-                                              self.option, 0)
+                    if self.check_file(f):
+                        print ('insert ' + f)
+                        self.db.insert_column('', self.hash_string(os.path.join(self.path, f)), os.path.join(self.path, f),self.option, 0)
 
         elif self.option == "f" and self.path != "":
             if self.check_file(self.path):
-                record_number=self.db.find_hash(self.hash_string(self.path))
-            if record_number[0][0] == 1:
+                record_number=self.db.find_hash(self.hash_string(self.path), self.path)
+            if record_number[0][0] > 0 and record_number[1][0] == 1:
                 previous_hash = self.print_info(record_number)
                 current_hash = self.hash_string(self.path)
                 if (compare_digest(previous_hash, current_hash)):
-                    print("Hash is same. Get hash updated ? \npress[Enter] to send or [n] to abort")
+                    print("Hash is same. Get hash updated ? \npress[Enter] to send to SAVAPI or [a] to abort")
                     str = input()
-                    if (str == 'n' or str.lower() == 'n'):
+                    if (str.lower() == 'a'):
                         print ('abort process')
                     elif (str == ''):
                         self.db.insert_column(self.hash_string(self.path), self.path, self.option, record_number[0][0])
@@ -101,48 +101,78 @@ class HashScan():
                     elif(str=='a'):
                         print ('finishing process...')
 
-            elif record_number[0][0] == 0:
+            elif record_number[0][0] == 0 and record_number[1][0] == 0:
                 print ('Check new file hash info : ' + self.path)
                 if self.check_file(self.path):
                     self.db.insert_column(self.hash_string(self.path), self.path, self.option,record_number[0][0])
-                print ('created hash information : ' + self.path + self.file_hash + '\n Get it sent to SAVAPI ? press[Enter] to send or [n] to abort')
-                str = input()
-                if (str == 'n' or str.lower() == 'n'):
-                    print ('abort process')
-                elif (str == ''):
-                    self.query_hash()
-
-        elif self.option == "d" and self.path != "":
-            checked_array = []
-            new_array = []
-            for f in self.find_files():
-                if self.check_file(f):
-                    hash = self.hash_string(f)
-                    record_number = self.db.find_hash(hash)
-                    if record_number[0][0] == 1:
-                        record_info = [record_number[0][0],f,hash,record_number[0][1]]
-                        checked_array.append(record_info)
-                    elif record_number[0][0] == 0:
-                        new_array.append(f)
-
-            if len(checked_array) > 0:
-                 self.check_dir(checked_array)
-
-            if len(new_array) > 0:
-                hash_array = []
-                for files in new_array:
-                    self.hash = self.hash_string(files)
-                    hash_array.append(self.hash)
-                    self.db.insert_column(self.hash, files, self.option, 0)
-                print ('created hash information : ' + self.path + '\n Get them sent to SAVAPI ? press[Enter] to send or [a] to abort')
+                print ('Created hash information : ' + self.path + ' : ' +self.file_hash + '\n Get it sent to SAVAPI ? press[Enter] to send or [a] to abort')
                 str = input()
                 if (str == 'a' or str.lower() == 'a'):
                     print ('abort process')
                 elif (str == ''):
-                    for self.hash in hash_array:
-                        self.query_hash()
-                else:
-                    print ("Invalid option... end the process")
+                    self.query_hash()
+            elif record_number[0][0] == 0 and record_number[1][0] > 0:
+                print ('Previous record of file exist, but hash is different')
+                print('File Path   : ' + self.path + ' | ' + 'Current File Hash   : ' + self.hash_string(self.path) + ' | Database File Hash  :' + record_number[1][1] + ' | Last Update : ' + record_number[1][2] )
+                str = self.check_suspicious_file()
+                if (str == 'q'):
+                    self.query_hash(self)
+                elif (str == 'u'):
+                    self.db.insert_column(record_number[1][1],self.hash_string(self.path), self.path, self.option, record_number[1][0])
+                elif (str == 'a'):
+                    print('finishing process...')
+
+        elif self.option == "d" and self.path != "":
+            checked_array = []
+            new_array = []
+            suspicious_array = []
+            for f in self.find_files():
+                if self.check_file(f):
+                    hash = self.hash_string(f)
+                    record_number = self.db.find_hash(hash, f)
+                    if record_number == None:
+                        print ('file name : '+ f + 'can not import this info will be saved as log')
+                        self.record_exception(f)
+                    elif record_number[0][0] > 0 and record_number[1][0] == 1:
+                        record_info = [record_number[0][0],f,hash,record_number[0][1],record_number[0][2]]
+                        checked_array.append(record_info)
+                    elif record_number[0][0] == 0 and record_number[1][0] == 0:
+                        new_array.append(f)
+                    elif record_number[0][0] == 0 and record_number[1][0] == 1:
+                        record_info = [record_number[0][0], f, hash, record_number[1][1], record_number[1][2]]
+                        suspicious_array.append(record_info)
+
+            if len(checked_array) > 0:
+                print ('Do you want to check current records and hash info ? Press Enter to proceed or press any key to abort')
+                str = input()
+                if (str == ''):
+                    self.check_dir(checked_array)
+
+            if len(new_array) > 0:
+                print('New files have been found, do you want to update the dataabse? Press Enter to proceed or press any key to abort')
+                str = input()
+                if (str == ''):
+                    hash_array = []
+                    for files in new_array:
+                        self.hash = self.hash_string(files)
+                        hash_array.append(self.hash)
+                        print ('insert :' + files)
+                        self.db.insert_column('', self.hash, files, self.option, 0)
+
+                    print ('created hash information \n Get them sent to SAVAPI ? press[Enter] to send or [a] to abort')
+                    str = input()
+                    if (str == 'a' or str.lower() == 'a'):
+                        print ('abort process')
+                    elif (str == ''):
+                        for self.hash in hash_array:
+                            self.query_hash()
+                    else:
+                        print ("Invalid option... end the process")
+
+            if len(suspicious_array) > 0:
+                print('These files exit in database as a file name , but hash is different')
+                for files in suspicious_array:
+                    self.check_dir(suspicious_array)
 
 
     def check_file(self,filepath):
@@ -225,45 +255,67 @@ class HashScan():
         previous_hash = record_info[0][1]
         return previous_hash
 
-    def check_dir(self,checked_array):
+    def check_dir(self,target_array):
         str = ""
-        while str == "":
-            for arr in checked_array:
-                    print("File Path   : " + arr[1]+ " | " + 'File Hash   : ' + arr[2] + " | " + 'Last Update : ' + arr[3])
 
-            print("Choose action")
-            print("Show all files and hash,  enter [p]")
-            print("Compear all hash info  ,  enter [c]")
-            print("Abort                  ,  enter [a]")
+        for arr in target_array:
+            print('File Path   : ' + arr[1] + ' | ' + 'Current File Hash   : ' + arr[2] + ' | Database File Hash  :' +
+                  arr[3] + ' | Last Update : ' + arr[4])
+
+        while str == "":
+
+            print('\nChoose action')
+            print('Show all files and hash       :  enter [p]')
+            print('Compear all hash info         :  enter [c]')
+            print('Insert all hash and file info :  enter [i]')
+            print('Update file hash              :  enter [u]')
+            print('Query all files to SAVAPI     :  enter [q]')
+            print('Abort                         :  enter [a]')
+
             str = input()
 
             if str.lower() == "p":
-                for arr in checked_array:
-                    print("File Path   : " + arr[0][0] + " | " + 'File Hash   : ' + arr[0][1] + " | " + 'Last Update : ' + arr[0][4])
+                for arr in target_array:
+                    print('File Path   : ' + arr[1]+ ' | ' + 'Current File Hash   : ' + arr[2] + ' | Database File Hash  :' +  arr[3] + ' | Last Update : ' + arr[4])
+                str = ''
             elif str.lower() == "c":
                 diff_hash = []
-                for arr in checked_array:
-                    if (compare_digest(arr[0][1], current_hash)) == False:
-                        hash_info = [arr[0][0],arr[0][1],current_hash]
+
+                for arr in target_array:
+                    if (compare_digest(arr[2], arr[3])) == False:
+                        hash_info = [arr[2],arr[3],arr[1]]
                         diff_hash.append(hash_info)
-                        print('Previous hash is ' + arr[0][1])
-                        print('Current  hash is ' + current_hash + '\n')
+                        print("Previous hash is " + arr[2] + " :  Current  hash is  "+ arr[3])
+                        str = self.check_suspicious_file()
 
-                str = self.check_suspicious_file()
-
-                if (str == 'q'):
-                    for hash_info in diff_hash:
-                        self.query_hash(self)
-                elif (str == 'u'):
-                    self.db.insert_column(self.hash_string(self.path), self.path, self.option, record_number[0][0])
-                elif (str == 'a'):
-                    print('finishing process...')
-            elif str.lower() =="a":
+                        if (str == 'q'):
+                            for hash_info in diff_hash:
+                                self.query_hash(self)
+                        elif (str == 'u'):
+                            self.db.insert_column('', self.hash_string(self.path), self.path, self.option, 1)
+                        elif (str == 'a'):
+                            print('finishing process...')
+                    else:
+                        print (arr[1] + " is same hash")
+                str = ''
+            elif str.lower() == 'i':
+                for arr in target_array:
+                    self.db.insert_column('',arr[2], arr[1], self.option, 0)
+                str = ''
+            elif str.lower() == "u":
+                self.hash = arr[2]
+                self.db.insert_column(arr[3], arr[2],arr[1], self.option, 1)
+                str = ''
+            elif str.lower() =='a':
                 print('finishing process...')
-                return None
             else:
                 print("Invalid option")
-                str = ""
+                str = ''
+
+    def record_exception(self,filename):
+        with open(LOG_PATH +'/error.log', 'rw') as f:
+            f.write(filename + '\n')
+
 
 
 class DatabaseInfo():
@@ -284,29 +336,39 @@ class DatabaseInfo():
         con = sqlite3.connect(self.db_path)
         return con
 
-    def insert_column(self, hash, filename, option, colum_number):
+    def insert_column(self, hash, new_hash,filename, option, colum_number):
         if option == 'i':
             cur = self.create_connection()
-            sqlstring = 'insert into HASH (FILE_NAME, HASH, SCANED_FLAG, INSERTED_DATE, UPDATED_DATE) values ("' + filename + '", "' + hash + '",0, "' + datetime.datetime.now().strftime(
+            sqlstring = 'insert into HASH (FILE_NAME, HASH, SCANED_FLAG, INSERTED_DATE, UPDATED_DATE) values ("' + filename + '", "' + new_hash + '",0, "' + datetime.datetime.now().strftime(
                 "%Y-%m-%d %H:%M:%S") + '","' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '")'
         elif option == 'f' or option == 'd':
             cur = self.create_connection()
             if colum_number == 0:
-                sqlstring = 'insert into HASH (FILE_NAME, HASH, SCANED_FLAG, INSERTED_DATE, UPDATED_DATE) values ("' + filename + '", "' + hash + '",0, "' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '","' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '")'
+                sqlstring = 'insert into HASH (FILE_NAME, HASH, SCANED_FLAG, INSERTED_DATE, UPDATED_DATE) values ("' + filename + '", "' + new_hash + '",0, "' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '","' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '")'
             elif colum_number >0:
-                sqlstring = 'update HASH set HASH="' + hash + '", SCANED_FLAG=1, UPDATED_DATE="'+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '" where HASH="' + hash + '"'
+                sqlstring = 'update HASH set HASH="' + new_hash + '", SCANED_FLAG=1, UPDATED_DATE="'+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '" where HASH="' + hash + '" and FILE_NAME="' + filename + '"'
+        try:
+            cur.execute(sqlstring)
+            cur.commit()
+            cur.close()
+        except sqlite3.Error as e:
+            print( filename + 'can not import' + e.args[0])
+            with open(LOG_PATH + '/error.log', 'a') as f:
+                f.write(filename + '\n')
 
-        cur.execute(sqlstring)
-        cur.commit()
-        cur.close()
 
-    def find_hash (self,hash):
+    def find_hash (self, hash, file_path):
         cur = self.create_connection()
-        sqlstring = 'select count(),UPDATED_DATE as record_number from HASH where HASH =' + '"' + hash + '"'
-        r = cur.execute(sqlstring)
-        record = r.fetchall()
-        cur.close()
-        return record
+        sqlstring = 'select count(),HASH, UPDATED_DATE from HASH where HASH =' + '"' + hash + '" UNION ALL select count(),HASH, UPDATED_DATE from HASH where FILE_NAME="' + file_path + '"'
+        try:
+            r = cur.execute(sqlstring)
+            record = r.fetchall()
+            cur.close()
+            return record
+        except sqlite3.Error as e:
+            print(file_path + 'can not import' + e.args[0])
+            with open(LOG_PATH + '/error.log', 'a') as f:
+                f.write(file_path + '\n')
 
     def select_hash(self, filename):
         cur = self.create_connection()
@@ -316,6 +378,7 @@ class DatabaseInfo():
         detail_hash = r.fetchall()
         r.close()
         return detail_hash
+
 
 if __name__ == '__main__':
     options = parser()
